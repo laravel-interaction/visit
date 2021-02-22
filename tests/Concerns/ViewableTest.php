89514ca801da37878c9d323d618f6049387c12f4
Zing <zingimmick@outlook.com>
2021-02-22 12:10:51 +0800

    Use support package for numberForHumans (#17)

--- tests/Concerns/ViewableTest.php
+++ tests/Concerns/ViewableTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Zing\LaravelEloquentView\Tests\Concerns;
 
-use Mockery;
 use Zing\LaravelEloquentView\Tests\Models\Subject;
 use Zing\LaravelEloquentView\Tests\Models\User;
 use Zing\LaravelEloquentView\Tests\TestCase;
@@ -45,39 +44,12 @@ class ViewableTest extends TestCase
         self::assertSame('2', $subject->viewsCountForHumans());
     }
 
-    public function data(): array
+    public function testViewersCountForHumans(): void
     {
-        return [
-            [0, '0', '0', '0'],
-            [1, '1', '1', '1'],
-            [12, '12', '12', '12'],
-            [123, '123', '123', '123'],
-            [12345, '12.3K', '12.35K', '12.34K'],
-            [1234567, '1.2M', '1.23M', '1.23M'],
-            [123456789, '123.5M', '123.46M', '123.46M'],
-            [12345678901, '12.3B', '12.35B', '12.35B'],
-            [1234567890123, '1.2T', '1.23T', '1.23T'],
-            [1234567890123456, '1.2Qa', '1.23Qa', '1.23Qa'],
-            [1234567890123456789, '1.2Qi', '1.23Qi', '1.23Qi'],
-        ];
-    }
-
-    /**
-     * @dataProvider data
-     *
-     * @param mixed $actual
-     * @param mixed $onePrecision
-     * @param mixed $twoPrecision
-     * @param mixed $halfDown
-     */
-    public function testViewersCountForHumans($actual, $onePrecision, $twoPrecision, $halfDown): void
-    {
-        $legacyMock = Mockery::mock(Subject::class);
-        $legacyMock->shouldReceive('viewersCountForHumans')->passthru();
-        $legacyMock->shouldReceive('viewersCount')->andReturn($actual);
-        self::assertSame($onePrecision, $legacyMock->viewersCountForHumans());
-        self::assertSame($twoPrecision, $legacyMock->viewersCountForHumans(2));
-        self::assertSame($halfDown, $legacyMock->viewersCountForHumans(2, PHP_ROUND_HALF_DOWN));
+        $user = User::query()->create();
+        $subject = Subject::query()->create();
+        $user->view($subject);
+        self::assertSame('1', $subject->viewsCountForHumans());
     }
 
     public function testIsViewedBy(): void
