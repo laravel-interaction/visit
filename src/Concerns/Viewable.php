4eff905b281a127a12f6a6be9888d6c82cd1ec84
zingimmick <zingimmick@outlook.com>
2021-02-22 12:09:17 +0800

    Use support package for numberForHumans

--- src/Concerns/Viewable.php
+++ src/Concerns/Viewable.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Http\Request;
+use LaravelInteraction\Support\Interaction;
 use function is_a;
 
 /**
@@ -81,9 +82,7 @@ trait Viewable
 
     public function viewsCountForHumans($precision = 1, $mode = PHP_ROUND_HALF_UP, $divisors = null): string
     {
-        $number = $this->viewsCount();
-
-        return $this->numberForHumans($number, $precision, $mode, $divisors);
+        return Interaction::numberForHumans($this->viewsCount(), $precision, $mode, $divisors ?? config('eloquent-view.divisors'));
     }
 
     public function loadViewersCount()
@@ -110,27 +109,9 @@ trait Viewable
         return (int) $this->viewers_count;
     }
 
-    protected function numberForHumans($number, $precision = 1, $mode = PHP_ROUND_HALF_UP, $divisors = null): string
-    {
-        $divisors = collect($divisors ?? config('eloquent-view.divisors'));
-        $divisor = $divisors->keys()->filter(
-            function ($divisor) use ($number) {
-                return $divisor <= abs($number);
-            }
-        )->last() ?? 1;
-
-        if ($divisor === 1) {
-            return (string) $number;
-        }
-
-        return number_format(round($number / $divisor, $precision, $mode), $precision) . $divisors->get($divisor);
-    }
-
     public function viewersCountForHumans($precision = 1, $mode = PHP_ROUND_HALF_UP, $divisors = null): string
     {
-        $number = $this->viewersCount();
-
-        return $this->numberForHumans($number, $precision, $mode, $divisors);
+        return Interaction::numberForHumans($this->viewersCount(), $precision, $mode, $divisors ?? config('eloquent-view.divisors'));
     }
 
     public function scopeWhereViewedBy(Builder $query, Model $user): Builder
